cf38262f8e433710fc6626a80f895ff43a792544
Ganesh <ganeshraveendranit@gmail.com>
2023-11-22 10:04:53 +0530

    initial commit

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "github.com/aws/aws-lambda-go/lambda"
+
 func main() {
+	lambda.Start(Handler)
+
+}
+
+type InputEvent struct {
+	Link string `json:"link"`
+	Key  string `json:"key"`
+}
+
+func Handler(event InputEvent) {
 
 }
