53747474bb738b99b445df87b1d28cfb7975d92c
Ganesh <ganeshraveendranit@gmail.com>
2023-11-23 00:05:13 +0530

    working on configuring s3

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/rganes5/awsLambda/aws_lambda_api-gateway_s3event
 
 go 1.20
+
+require github.com/aws/aws-sdk-go v1.48.2
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
+
+var s3session *s3.S3
+
+const (
+	REGION      = "ap-south-1"
+	BUCKET_NAME = "lambdatest"
+)
+
+func init() {
+	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(REGION),
+	})))
+}
 
 func main() {
 	lambda.Start(Handler)
 
+}
+
+func Handler(event InputEvent) (string, error) {
+	fmt.Println("Function invoked!")
+	log.Println("Yay")
+	image := GetImage(event.Link)
+	_, err := s3session.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(image),
+		Bucket: aws.String(BUCKET_NAME),
+		Key:    aws.String(event.Key),
+	})
+	if err != nil {
+		return "Something went wrong", err
+	}
+	return "Everything worked YAY!", err
 }
 
 type InputEvent struct {
@@ -17,7 +51,16 @@ type InputEvent struct {
 	Key  string `json:"key"`
 }
 
-func Handler(event InputEvent) {
-	fmt.Println("Function invoked!")
-	log.Println("Yay")
+func GetImage(url string) (bytes []byte) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("get err", err)
+	}
+	defer resp.Body.Close()
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read all err", err)
+	}
+	return bytes
 }
